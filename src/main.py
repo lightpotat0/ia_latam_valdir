25ebab4733d12708f196907c0e6b5369f1108166
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2026-04-24 12:25:06 -0300

    limpando o codigo

--- src/main.py
+++ src/main.py
@@ -4,12 +4,8 @@ from dotenv import load_dotenv
 from pathlib import Path
 import os
 
-# 🔥 força o caminho correto do .env
 env_path = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
-
-# debug (pode remover depois)
-print("API KEY:", os.getenv("GROQ_API_KEY"))
 
 from langchain_groq import ChatGroq
 
